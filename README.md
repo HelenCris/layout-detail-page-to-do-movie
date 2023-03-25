d8c7451fac1f7ccc3f1231293ed0bd41d63cf442
Helen Cris <helencrisfernandes@hotmail.com>
2023-03-25 17:39:15 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## ToDoMovies - layout detail page
---
+++ android/app/src/main/kotlin/com/example/layout_detail_film_to_do_moveis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.layout_detail_film_to_do_moveis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
