90eb41bca112426afd1aa1bcb41a139d775b82dd
Helen Cris <helencrisfernandes@hotmail.com>
2023-03-26 21:06:17 -0300

    refactor: refactor code

--- lib/app/models/film_model.dart
+++ lib/app/models/film_model.dart
@@ -2,34 +2,30 @@
 import 'dart:convert';
 
 class FilmModel {
-  final String image;
   final String title;
   final int vote_count;
   final double popularity;
-  final List genres;
 
-  FilmModel(this.image, this.title, this.vote_count, this.popularity,
-      this.genres); // pegar name de cada genre
+  FilmModel(
+    this.title,
+    this.vote_count,
+    this.popularity,
+  );
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'backdrop_path': image,
       'title': title,
       'vote_count': vote_count,
       'popularity': popularity,
-      'genres': genres,
     };
   }
 
   factory FilmModel.fromMap(Map<String, dynamic> map) {
     return FilmModel(
-        map['backdrop_path'] as String,
-        map['title'] as String,
-        map['vote_count'] as int,
-        map['popularity'] as double,
-        List.from(
-          (map['genres'] as List),
-        ));
+      map['title'] as String,
+      map['vote_count'] as int,
+      map['popularity'] as double,
+    );
   }
 
   String toJson() => json.encode(toMap());
