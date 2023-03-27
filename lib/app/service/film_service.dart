787c30316375faebf992cea9ad0f448cebe29251
Helen Cris <helencrisfernandes@hotmail.com>
2023-03-26 21:14:57 -0300

    refactor: refactor code and add get

--- lib/app/service/film_service.dart
+++ lib/app/service/film_service.dart
@@ -1,31 +1,18 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:get/get.dart';
 
 import '../models/film_model.dart';
 
-class FilmService {
+class FilmService extends GetxController {
   final dio = Dio();
 
   final String _url =
       'https://api.themoviedb.org/3/movie/14836?api_key=fb1c070ef195d5d723198885c0b83632&language=en-US';
 
   Future<FilmModel> getFilm() async {
-    Response response = await dio.get(_url);
-
-    if (response.statusCode == 200) {
-      var encoded = jsonEncode(response.data);
-
-      var films = FilmModel.fromJson(encoded);
-
-      return films;
-    } else {
-      throw Exception('Failed to load');
-    }
-  }
-
-  Future<FilmModel> getSimilarFilm() async {
-    Response response = await dio.get('$_url/similar');
+    var response = await dio.get(_url);
 
     if (response.statusCode == 200) {
       var encoded = jsonEncode(response.data);
